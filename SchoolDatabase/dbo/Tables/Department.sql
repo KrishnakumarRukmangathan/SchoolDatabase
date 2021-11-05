6edab99aa21811273916fe753028ba84a491d4b8
Krishnakumar Rukmangathan <krishnakumr@gmail.com>
2021-11-05 18:42:05 +0530

    Update Department.sql

--- SchoolDatabase/dbo/Tables/Department.sql
+++ SchoolDatabase/dbo/Tables/Department.sql
@@ -3,7 +3,7 @@
     [Name]          NVARCHAR (50) NOT NULL,
     [Budget]        MONEY         NULL,
     [StartDate]     DATETIME      NOT NULL,
-    [Administrator] INT           NULL,
+    [Administrator] INT           NOT NULL,
     CONSTRAINT [PK_Department] PRIMARY KEY CLUSTERED ([DepartmentID] ASC)
 );
 
