efa1d9eecb52f16990cb41ae4cd3c5f004586c8e
Krishnakumar Rukmangathan <krishnakumr@gmail.com>
2021-11-05 18:08:40 +0530

    InitCommit1

--- SchoolDatabase/dbo/Tables/Department.sql
+++ SchoolDatabase/dbo/Tables/Department.sql
@@ -1,7 +1,7 @@
 ﻿CREATE TABLE [dbo].[Department] (
     [DepartmentID]  INT           NOT NULL,
     [Name]          NVARCHAR (50) NOT NULL,
-    [Budget]        MONEY         NOT NULL,
+    [Budget]        MONEY         NULL,
     [StartDate]     DATETIME      NOT NULL,
     [Administrator] INT           NULL,
     CONSTRAINT [PK_Department] PRIMARY KEY CLUSTERED ([DepartmentID] ASC)
