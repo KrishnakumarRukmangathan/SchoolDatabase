52bde7435e9968b8a6f2d04963c06207e2817318
Krishnakumar Rukmangathan <krishnakumr@gmail.com>
2021-11-07 17:22:04 +0530

    Update OnlineCourse.sql

--- SchoolDatabase/dbo/Tables/OnlineCourse.sql
+++ SchoolDatabase/dbo/Tables/OnlineCourse.sql
@@ -1,6 +1,7 @@
 ﻿CREATE TABLE [dbo].[OnlineCourse] (
     [CourseID] INT            NOT NULL,
-    [URL]      NVARCHAR (100) NULL,
+    [URL]      NVARCHAR (100) NOT NULL,
+    [URL2]      NVARCHAR (100) NULL,
     CONSTRAINT [PK_OnlineCourse] PRIMARY KEY CLUSTERED ([CourseID] ASC),
     CONSTRAINT [FK_OnlineCourse_Course] FOREIGN KEY ([CourseID]) REFERENCES [dbo].[Course] ([CourseID])
 );
